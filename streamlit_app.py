36f6be7eaaaf6b314fe5858b28ffd14610d8ff92
Gopesh <90859777+Gopesh223@users.noreply.github.com>
2025-02-09 17:02:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,24 @@
 import streamlit as st
 import os
-#import pyttsx3
-#import faiss
-#import pickle
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_ollama import OllamaEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 from streamlit_mic_recorder import speech_to_text
 
 pdf_path = "hp.pdf"
-vectorstore_dir = "faiss_index"
 
-embeddings = OllamaEmbeddings(model='llama3.2')
 model = OllamaLLM(model='llama3.2')
 
-os.makedirs(vectorstore_dir, exist_ok=True)
-
-if os.path.exists(os.path.join(vectorstore_dir, "index.faiss")):
-    vector_store = FAISS.load_local(vectorstore_dir, embeddings, allow_dangerous_deserialization=True)
-else:
+def load_pdf():
     loader = PDFPlumberLoader(pdf_path)
     documents = loader.load()
-    
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
     chunked_documents = text_splitter.split_documents(documents)
+    return chunked_documents
 
-    vector_store = FAISS.from_documents(chunked_documents, embeddings)
+documents = load_pdf()
 
-    vector_store.save_local(vectorstore_dir)
-
-def retrieve_docs(query):
-    return vector_store.similarity_search(query)
-
-def answer_question(question, documents):
+def answer_question(question):
     context = "\n\n".join([doc.page_content for doc in documents])
     template = """
     You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question.
@@ -46,9 +29,7 @@ def answer_question(question, documents):
     
     Answer:
     """
-    prompt = ChatPromptTemplate.from_template(template)
-    chain = prompt | model
-    return chain.invoke({"question": question, "context": context})
+    return model.invoke({"question": question, "context": context})
 
 st.title("The Sorcerer's Stone Chatbot")
 
@@ -68,12 +49,8 @@ if text:
 def run_query(question):
     if question:
         st.chat_message("user").write(question)
-        related_documents = retrieve_docs(question)
-        answer = answer_question(question, related_documents)
+        answer = answer_question(question)
         st.chat_message("assistant").write(answer)
-        #engine=pyttsx3.init()
-        #engine.say(answer)
-        #engine.runAndWait()
 
 if st.button("Use Speech Input"):
     if state.text_received:
@@ -82,4 +59,3 @@ if st.button("Use Speech Input"):
 question = st.chat_input("Ask your question")
 if question:
     run_query(question)
-
